d96580309df4c56edf1984f9de2b35c3410a995e
sc174971 <ss@sss.com>
2024-05-08 22:05:25 -0300

    modulo

--- main.tf
+++ main.tf
@@ -5,11 +5,3 @@ instance_type  = "t2.medium"
 instance_name = "Instancia1"
 
 }
-
-module "instancias" {
-source = "./modules/instancias"
-count = 1
-instance_type  = "t2.medium"
-instance_name = "Instancia2"
-
-}
