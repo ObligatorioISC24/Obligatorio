a486f4a0fe9d69311ab11fd06994096b99047b18
sc174971 <162226492+sc174971@users.noreply.github.com>
2024-06-03 13:53:25 -0300

    Delete Instancias.tf

--- Instancias.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "InstanciaDesdeTerraform" {
-  ami                    = "ami-051f8a213df8bc089"
-  instance_type          = "t2.micro"
-  key_name               = "Terraformkey"
-  tags = {
-    Name      = "InstanciaDesdeTerraform"
-    Terraform = "True" 
-  }
-}
