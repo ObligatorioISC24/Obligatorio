021bc24fbcc21a8c54f8e8ec042f582929234cac
sc174971 <162226492+sc174971@users.noreply.github.com>
2024-06-03 13:52:37 -0300

    Delete modules/instancias directory

--- modules/instancias/main-instancias.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "InstanciaDesdeTerraform" {
-  ami                    = "ami-051f8a213df8bc089"
-  instance_type          = var.instance_type
-  key_name               = "Terraformkey"
-  tags = {
-    Name      = var.instance_name
-    Terraform = "True" 
-  }
-}
