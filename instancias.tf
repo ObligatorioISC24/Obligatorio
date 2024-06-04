c7c6d19c044e68467e6e1ecab99cf43a68e7a264
root <root@ubuntu.myguest.virtualbox.org>
2024-06-03 22:14:24 -0300

    servidor backup

--- instancias.tf
+++ instancias.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "webapp-server01" {
     type        = "ssh"
     user        = "ec2-user"
     host        = self.public_ip
-    private_key = file("/home/santiago/Terraformkey.pem") ##cambiar aca por la ubicacion de la clave en tu pc
+    private_key = file("/home/Terraformkey.pem") ##cambiar aca por la ubicacion de la clave en tu pc
   }
 
   provisioner "remote-exec" {
@@ -51,7 +51,7 @@ resource "aws_instance" "webapp-server02" {
     type        = "ssh"
     user        = "ec2-user"
     host        = self.public_ip
-    private_key = file("/home/santiago/Terraformkey.pem") ##cambiar aca por la ubicacion de la clave en tu pc
+    private_key = file("/home/Terraformkey.pem") ##cambiar aca por la ubicacion de la clave en tu pc
   }
 
   provisioner "remote-exec" {
@@ -69,6 +69,42 @@ resource "aws_instance" "webapp-server02" {
       "sudo mkdir /var/www/documentos",
       "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.obligatorio-efs.dns_name}:/   /var/www/documentos"
 
+    ]
+  }
+}
+## BACKUP
+resource "aws_instance" "backup-server" {
+  ami                    = "ami-051f8a213df8bc089"
+  depends_on             = [aws_db_instance.obligatorio-rds]
+  instance_type          = "t2.micro"
+  key_name               = "Terraformkey" ##Revisar que exista esta clave en AWS
+  vpc_security_group_ids = [aws_security_group.obligatorio-sg.id]
+  subnet_id              = aws_subnet.obligatorio-tf-subnet-a.id
+  tags = {
+    Name      = "backup-server"
+    Terraform = "True"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("/home/Terraformkey.pem") ##cambiar aca por la ubicacion de la clave en tu pc
+  }
+
+  #Definicion de volumen para BACKUP
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_type = "gp2"
+    volume_size = 50
+    delete_on_termination = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkfs -t ext4 /dev/sdh && sudo mkdir /mnt/backup && sudo mount /dev/sdh /mnt/backup ",
+      "sudo mkdir /mnt/nfs && sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.obligatorio-efs.dns_name}:/   /mnt/nfs",
+      "echo '0 23 * * * root cp -R /mnt/nfs/* /mnt/backup' | sudo tee -a /etc/crontab",
+      "sudo systemctl restart crond"
     ]
   }
 }
