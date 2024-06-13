8c6d70d601d6fd9e58764803a84e6a807456473f
sc174971 <ss@sss.com>
2024-06-13 15:26:41 -0300

    arreglo de permisos

--- instancias.tf
+++ instancias.tf
@@ -25,19 +25,18 @@ resource "aws_instance" "webapp-server01" {
       "sudo yum-config-manager --enable remi-php54",
       "sudo yum install -y php php-cli php-common php-mbstring php-xml php-mysql php-fpm",
       "sudo yum install -y httpd",
-      "sudo systemctl start httpd",
       "sudo yum install -y git",
       "sudo yum install -y mariadb.x86_64",
-      "sudo chmod 777 /var/www/html",
       "sudo mkdir /var/www/html/img && sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.obligatorio-efs.dns_name}:/   /var/www/html/img", #montaje de EFS en directorio utilizado para las imagenes de la app
       "git clone https://github.com/ObligatorioISC24/ecommerce.git", #clonado del repo en el home para poder copiar las imagenes hacia el NFS
       "sudo mv /home/ec2-user/ecommerce/img/* /var/www/html/img/ && rm -r /home/ec2-user/ecommerce/img/", #movimiento de las imagenes y borrado de la carpeta img para no copiarla vacia en el siguiente punto
       "sudo mv /home/ec2-user/ecommerce/* /var/www/html/", #movimiento de la app al directorio correcto
       "sudo sed -i 's/localhost/${aws_db_instance.obligatorio-rds.address}/' /var/www/html/config.php", #Reemplazo de la url de la base de datos
       "sudo sed -i \"s/define('DB_PASSWORD', 'root')/define('DB_PASSWORD', '${var.DB_PASSWORD}')/\" /var/www/html/config.php", # reemplazo de la password
+      "sudo chown -R apache:apache /var/www", #cambio de usuario y grupo por el de apache
       "wget https://raw.githubusercontent.com/ObligatorioISC24/ecommerce/main/dump.sql",
       "sudo mysql -h ${aws_db_instance.obligatorio-rds.address} -u ${var.DB_USER} -p${var.DB_PASSWORD} ${var.DB_DATABASE} < dump.sql", #volcado del dump en la base
-
+      "sudo systemctl start httpd"
     ]
   }
 }
@@ -69,19 +68,18 @@ resource "aws_instance" "webapp-server02" {
       "sudo yum-config-manager --enable remi-php54",
       "sudo yum install -y php php-cli php-common php-mbstring php-xml php-mysql php-fpm",
       "sudo yum install -y httpd",
-      "sudo systemctl start httpd",
       "sudo yum install -y git",
       "sudo yum install -y mariadb.x86_64",
-      "sudo chmod 777 /var/www/html",
-      "sudo mkdir /var/www/html/img && sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.obligatorio-efs.dns_name}:/   /var/www/html/img",
-      "git clone https://github.com/ObligatorioISC24/ecommerce.git",
-      "sudo mv /home/ec2-user/ecommerce/img/* /var/www/html/img/ && rm -r /home/ec2-user/ecommerce/img/",
-      "sudo mv /home/ec2-user/ecommerce/* /var/www/html/",
-      "sudo sed -i 's/localhost/${aws_db_instance.obligatorio-rds.address}/' /var/www/html/config.php",
-      "sudo sed -i \"s/define('DB_PASSWORD', 'root')/define('DB_PASSWORD', '${var.DB_PASSWORD}')/\" /var/www/html/config.php",
+      "sudo mkdir /var/www/html/img && sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.obligatorio-efs.dns_name}:/   /var/www/html/img", #montaje de EFS en directorio utilizado para las imagenes de la app
+      "git clone https://github.com/ObligatorioISC24/ecommerce.git", #clonado del repo en el home para poder copiar las imagenes hacia el NFS
+      "sudo mv /home/ec2-user/ecommerce/img/* /var/www/html/img/ && rm -r /home/ec2-user/ecommerce/img/", #movimiento de las imagenes y borrado de la carpeta img para no copiarla vacia en el siguiente punto
+      "sudo mv /home/ec2-user/ecommerce/* /var/www/html/", #movimiento de la app al directorio correcto
+      "sudo sed -i 's/localhost/${aws_db_instance.obligatorio-rds.address}/' /var/www/html/config.php", #Reemplazo de la url de la base de datos
+      "sudo sed -i \"s/define('DB_PASSWORD', 'root')/define('DB_PASSWORD', '${var.DB_PASSWORD}')/\" /var/www/html/config.php", # reemplazo de la password
+      "sudo chown -R apache:apache /var/www", #cambio de usuario y grupo por el de apache
       "wget https://raw.githubusercontent.com/ObligatorioISC24/ecommerce/main/dump.sql",
-      "sudo mysql -h ${aws_db_instance.obligatorio-rds.address} -u ${var.DB_USER} -p${var.DB_PASSWORD} ${var.DB_DATABASE} < dump.sql", 
-      "sudo systemctl restart httpd"
+      "sudo mysql -h ${aws_db_instance.obligatorio-rds.address} -u ${var.DB_USER} -p${var.DB_PASSWORD} ${var.DB_DATABASE} < dump.sql", #volcado del dump en la base
+      "sudo systemctl start httpd"
       
     ]
   }
